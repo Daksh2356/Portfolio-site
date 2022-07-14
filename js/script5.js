b191f812321281e2ace2ad57b65c498e8e377c9b
daksh <dakshmakhija@gmail.com>
2022-07-14 23:54:57 +0530

    commits

--- js/script5.js
+++ js/script5.js
@@ -1,4 +1,3 @@
-console.log("Script running");
 document.querySelector('.cross').style.display='none';
 document.querySelector('.hamburger').addEventListener('click',()=>{
     document.querySelector('.sidebar').classList.toggle('sidebarGo');
